f5e1c039e4d683e8fb8feb34e9d88472ddf2a2a2
Tony2464 <anthony.fargette@yahoo.fr>
2020-06-09 18:58:57 +0200

    ADD homepage and css

---
+++ PHP-Website/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homepage</title>
+    <link rel="icon" sizes="32x32" type="image/png" href="img/favicon.png" />
+    <link rel="stylesheet" href="css/custom-bootstrap.css">
+</head>
+
+<body>
+    <main>
+        <div class="h1 text-center">Welcome to Recidge !</div>
+    </main>
+</body>
+
+</html>
