4fad78e5d08eaf2ea158e19335a9c1f4c7513aba
Tony2464 <anthony.fargette@yahoo.fr>
2020-06-17 23:21:58 +0200

    ADD Floating action buttons

--- Android-App/app/src/main/java/com/tony2464/recidge/IngredientsFragment.java
+++ Android-App/app/src/main/java/com/tony2464/recidge/IngredientsFragment.java
@@ -9,10 +9,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class IngredientsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ingredients, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipes, container, false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        return view;
     }
 }
